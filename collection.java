9a4689335eacf63a8d2ce7a67b5b6f55b3057be9
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-24 22:27:40 +0530

    Added:WelcomeCollection.java

---
+++ collection.java
@@ -0,0 +1,10 @@
+package addressBookCollection;
+
+public class WelcomeCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to java");
+	}
+
+}
