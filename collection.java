962dfcb013e3bf3757bd6db585ed0994b70c75bd
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-24 22:38:19 +0530

    Added:AddressBookUC6.java, add unique name

--- collection.java
+++ collection.java
@@ -1,27 +1,39 @@
 package addressBookCollection;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
-public class AddressBook5 {
+public class AddressBookUC6 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   
-		ArrayList<String> pappu=new ArrayList<String>();  
-		
-		pappu.add("Aryan");
-		pappu.add("Aahan");
-		pappu.add("Shakshi");
-		pappu.add("Ganesh");
-		pappu.add("Vinod");
+	ArrayList<String> pappu = new ArrayList <String>();  
+	    
+//		  pappu.add("Aryan");
+//	      pappu.add("Aahan");
+//	      pappu.add("Ganesh");
+//	      pappu.add("Shakshi");
+//	      pappu.add("Rahul");
+//	      pappu.add("Raj");
 	  
-	      System.out.println(pappu);
+	      //displaying elements
+	      //System.out.println(pappu);
 	  
-	      //Adding trough index value
-	      pappu.add(3, "Aryan");
+	      //Adding name trough index
+	      pappu.add(0, "Aryan");
+	      pappu.add(1, "Aahan");
+	      pappu.add(2, "Ganesh");
+	      pappu.add(3, "Shakshi");
+	      pappu.add(4, "Rahul");
+	      pappu.add(5, "Raj");
 	  
 	      //displaying elements
 	      System.out.println(pappu);
-	   }  
+	      
+	      pappu.add(4, "pappu");
+	      System.out.println(pappu);
+
+	   } 
 	}
 
 
