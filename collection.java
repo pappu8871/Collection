d56e32a4e32ba53a2c8395d480c6352e068249d6
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-24 22:32:31 +0530

    Added:AddressBook5.java, add multiple person

--- collection.java
+++ collection.java
@@ -1,10 +1,27 @@
 package addressBookCollection;
 
-public class WelcomeCollection {
+import java.util.ArrayList;
 
+public class AddressBook5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome to java");
+   
+		ArrayList<String> pappu=new ArrayList<String>();  
+		
+		pappu.add("Aryan");
+		pappu.add("Aahan");
+		pappu.add("Shakshi");
+		pappu.add("Ganesh");
+		pappu.add("Vinod");
+	  
+	      System.out.println(pappu);
+	  
+	      //Adding trough index value
+	      pappu.add(3, "Aryan");
+	  
+	      //displaying elements
+	      System.out.println(pappu);
+	   }  
 	}
 
-}
+
